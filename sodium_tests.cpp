bc29aaa20afc23141b030e3ebe1357c6dac93b36
Robert Oleksy <robertoleksy6@gmail.com>
2017-11-06 14:45:18 +0000

    test

--- sodium_tests.cpp
+++ sodium_tests.cpp
@@ -9,10 +9,16 @@
 #include <cmath>
 #include <condition_variable>
 #include <mutex>
+#include <cassert>
+#include <numeric>
+
+void escape(void* p) {
+  asm volatile("" : : "g"(p) : "memory");
+}
 
 // F = void(buffin*, buffout*, buffin_size, buffout_size) F have to be thread-safe
 
-template<typename F, size_t max_buff_size = 65536*2, size_t max_threads_count = 16>
+template<typename F, size_t max_buff_size = 65536, size_t max_threads_count = 16>
 class crypto_test {
 public:
     crypto_test(F fun, std::string file_name);
@@ -38,26 +44,27 @@ void crypto_test<F, max_buff_size, max_threads_count>::test_buffer_size(std::fun
 	std::condition_variable trigger_cv; // if notifyed all threads starts
 	std::mutex thread_mutex;
 	bool threads_started = false; // protected by thread_mutex
-    constexpr size_t iterations = 1000;
+    constexpr size_t iterations = 20000;
 
     auto thread_lambda = [&init_buffer_lambda, &thread_results, &iterations, this, &thread_cv, &thread_mutex, &threads_started, &trigger_cv, &thread_ready_flag]
-	(size_t buff_size, size_t return_array_index) {
+	(size_t buff_size, size_t thread_index, size_t number_of_concurrent_threads) {
+		assert(number_of_concurrent_threads > 0);
         std::vector<unsigned char> inbuff(buff_size);
         std::vector<unsigned char> outbuff(buff_size);
         init_buffer_lambda(inbuff.data(), outbuff.data(), buff_size, buff_size);
 
 		std::unique_lock<std::mutex> lg(thread_mutex);
-		thread_ready_flag.at(return_array_index) = true;
-		thread_cv.at(return_array_index).notify_all(); // i am readry to work
+		thread_ready_flag.at(thread_index) = true;
+		thread_cv.at(thread_index).notify_all(); // i am readry to work
 		trigger_cv.wait(lg, [&threads_started]{return threads_started;}); // wait for start
 
         auto start_point = std::chrono::steady_clock::now();
-        for (size_t j=0; j<iterations; j++)
+        for (size_t j=0; j<iterations/number_of_concurrent_threads; j++)
         {
             m_test_fun(inbuff.data(), outbuff.data(), buff_size, max_buff_size);
         }
         auto end_point = std::chrono::steady_clock::now();
-        thread_results.at(return_array_index) = std::chrono::duration_cast<std::chrono::nanoseconds>(end_point - start_point).count();
+        thread_results.at(thread_index) = std::chrono::duration_cast<std::chrono::nanoseconds>(end_point - start_point).count();
     };
 
 
@@ -69,7 +76,7 @@ void crypto_test<F, max_buff_size, max_threads_count>::test_buffer_size(std::fun
 			thread_ready_flag.fill(false); // all threads are not ready
             for (size_t j=0; j<=threads_count; j++) // iterate through threads count
             {
-                threads.at(j) = std::move(std::thread(thread_lambda, i, j));
+                threads.at(j) = std::move(std::thread(thread_lambda, i, j, threads_count+1));
             }
 
 			for (size_t j=0; j<=threads_count; j++) { // waiting for all threads
@@ -94,7 +101,7 @@ void crypto_test<F, max_buff_size, max_threads_count>::test_buffer_size(std::fun
 
             size_t time_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_point - start_point).count();
             // buff_size threads_count time_ms
-            m_file << i << '\t' << threads_count << '\t' << time_ms/static_cast<double>(iterations*(threads_count+1)) << '\n';
+            m_file << i << '\t' << threads_count << '\t' << time_ms/static_cast<double>(iterations) << '\n';
 
 //            m_file << i << '\t' << threads_count+1 << '\t' << (time_ms/static_cast<double>(iterations))/((threads_count+1)*(threads_count+1))<< '\n'; // buff_size threads_count time_ms
             // threads_count+1 becouse array index starts from 0
@@ -114,7 +121,9 @@ int main() {
     std::fill_n(key, crypto_onetimeauth_KEYBYTES, 0xfd);
 //     crypto_onetimeauth_keygen(key); // was introduced in libsodium 1.0.12.
     auto poly = [&key](unsigned char* buff_in, unsigned char* buff_out, size_t in_size, size_t out_size ){
+		escape(buff_out);
         crypto_onetimeauth(buff_out, buff_in, in_size, key);
+		escape(buff_out);
     };
     {
         crypto_test<decltype(poly)> crypt(poly, "poly_auth_results.txt");
@@ -123,10 +132,12 @@ int main() {
 
     // poly verify
     auto poly_verify = [&key](unsigned char* buff_in, unsigned char* buff_out, size_t in_size, size_t out_size ){
+		escape(buff_out);
         if (crypto_onetimeauth_verify(buff_out, buff_in, in_size, key) != 0) {
            std::cerr << "Poly verification fail" << std::endl;
            std::abort();
         }
+		escape(buff_out);
     };
     auto init_buffers = [&poly, &key](unsigned char* buff_in, unsigned char* buff_out, size_t in_size, size_t out_size ){
         std::fill_n(buff_in, in_size, 0xfd);
